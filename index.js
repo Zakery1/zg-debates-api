50140c667b12e022584bde2f8e494dee64d951b5
Zachary Graham <zakgraham@gmail.com>
2020-12-08 23:07:50 -0700

    replace massive with node-postgres

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 var cors = require("cors");
 
-require("dotenv").config();
+const { Pool, Client } = require("pg");
 
-const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 
-
 app.options("*", cors());
+
+app.use(bodyParser.json());
 
 app.use(
   cors({
@@ -18,31 +19,24 @@ app.use(
   })
 );
 
-const sequelize = new Sequelize({
-  database: process.env.DB_DATABASE,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: 5432,
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
   },
 });
 
-const testConnection = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-};
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, "Result", res);
+  pool.end();
+});
 
-testConnection();
+
 
 app.get("/", (req, res) => {
   res.send("welcome to new twitter ");
