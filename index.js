713f0108d9c2f2ccd69bd5bb382c777e4a7d8701
Zachary Graham <zakgraham@gmail.com>
2020-12-16 23:12:20 -0700

    add cors whitelist

--- index.js
+++ index.js
@@ -12,10 +12,21 @@ app.options("*", cors());
 
 app.use(bodyParser.json());
 
+var whitelist = ['https://zg-debates.netlify.app/', 'http://localhost:8080']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 app.use(
   cors({
     credentials: true,
-    origin: true,
+    origin: corsOptions,
   })
 );
 
