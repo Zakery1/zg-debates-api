05a6ad497df249c41fbf8e6284f3fd033bf67b03
Zachary Graham <zakgraham@gmail.com>
2021-02-17 15:08:01 -0700

    fix fetchvotes contributions endpoint

--- routers/contributions.router.js
+++ routers/contributions.router.js
@@ -1,82 +1,84 @@
+const router = require("express").Router();
+const { pool } = require("../helpers/pool.helper");
 
-const router = require('express').Router();
-const { pool } = require('../helpers/pool.helper');
-
+//gets all contributions for current discussion
 router.get("/contributions/:discussionId", (request, response) => {
-    const { discussionId } = request.params;
-    pool.query(
-      `SELECT * FROM contributions WHERE discussion_id = ${discussionId} order by points Desc;`,
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        const contributions = results.rows.map((contribution) => {
-          return {
-            id: contribution.id,
-            userId: contribution.user_id,
-            discussionId: contribution.discussion_id,
-            contribution: contribution.contribution,
-            agree: contribution.agree,
-            neutral: contribution.neutral,
-            disagree: contribution.disagree,
-            points: contribution.points,
-          };
-        });
-        response.status(200).json(contributions);
+  const { discussionId } = request.params;
+  pool.query(
+    `SELECT * FROM contributions WHERE discussion_id = ${discussionId} order by points Desc;`,
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    );
+      const contributions = results.rows.map((contribution) => {
+        return {
+          id: contribution.id,
+          userId: contribution.user_id,
+          discussionId: contribution.discussion_id,
+          contribution: contribution.contribution,
+          agree: contribution.agree,
+          neutral: contribution.neutral,
+          disagree: contribution.disagree,
+          points: contribution.points,
+        };
+      });
+      response.status(200).json(contributions);
+    }
+  );
 });
-  
-router.get("/contributions/:userId", (request, response) => {
-    let { userId } = request.params;
-  
-    pool.query(
-      `select contribution_id from votes where user_id = ${userId};`,
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        const votes = results.rows.map((vote) => {
-          return { userId: vote.user_id, contributionId: vote.contribution_id };
-        });
-        response.status(200).json(votes);
+
+router.get("/contributions/", (request, response) => {
+  console.log("New check for hitting votes", request.query);
+  let { userId } = request.query;
+
+  pool.query(
+    `select contribution_id from votes where user_id = ${userId};`,
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    );
+      const votes = results.rows.map((vote) => {
+        return { userId: vote.user_id, contributionId: vote.contribution_id };
+      });
+      response.status(200).json(votes);
+    }
+  );
 });
-  
-  ////voting
-  
+
+////voting
+
 router.put("/contributions", (request, response) => {
-    let { contributionId } = request.body;
-  
-    pool.query(
-      `UPDATE contributions SET points = points - 1 WHERE id = $1;`,
-      [contributionId],
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        response.status(200).json("Vote Removed");
+  let { contributionId } = request.body;
+
+  pool.query(
+    `UPDATE contributions SET points = points - 1 WHERE id = $1;`,
+    [contributionId],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    );
+      response.status(200).json("Vote Removed");
+    }
+  );
 });
-  
+
 router.put("/contributions", (request, response) => {
-    let { contributionId } = request.body;
-  
-    pool.query(
-      `UPDATE contributions SET points = points + 1 WHERE id = $1;`,
-      [contributionId],
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        response.status(200).json("New vote counted");
+  let { contributionId } = request.body;
+
+  pool.query(
+    `UPDATE contributions SET points = points + 1 WHERE id = $1;`,
+    [contributionId],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    );
+      response.status(200).json("New vote counted");
+    }
+  );
 });
 
 router.post("/contributions", (request, response) => {
+  console.log("post new contribution stuff", request.body);
   let {
     userId,
     discussionId,
@@ -131,22 +133,24 @@ router.put("/contributions/:id", (request, response) => {
   );
 });
 
-router.get("/contributions/:id", (request, response) => {
-  const id = request.params.id;
+// router.get("/contributions/:id", (request, response) => {
+//   console.log("newer one, cotributions, looking for votes", request.params);
+//   const id = request.params.id;
 
-  pool.query(
-    "SELECT * FROM contributions WHERE id = $1",
-    [id],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      const contribution = results.rows.map((contribution) => {
-        return contribution.contribution;
-      });
-      response.status(200).json(contribution[0]);
-    }
-  );
-});
+//   pool.query(
+//     "SELECT * FROM contributions WHERE id = $1",
+//     [id],
+//     (error, results) => {
+//       if (error) {
+//         throw error;
+//       }
+//       const contribution = results.rows.map((contribution) => {
+//         console.log("I wanna see this contributions get", contribution);
+//         return contribution.contribution;
+//       });
+//       response.status(200).json(contribution[0]);
+//     }
+//   );
+// });
 
 module.exports = router;
